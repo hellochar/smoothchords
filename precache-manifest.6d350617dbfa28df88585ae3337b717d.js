e90d8cc82cbedf209b255f64e981b802b04f8a6c
Xiaohan Zhang <hellocharlien@hotmail.com>
2019-05-08 02:02:37 -0700

    Updates

---
+++ precache-manifest.6d350617dbfa28df88585ae3337b717d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adbb9ff755da7d6d99859fb1963c58f9",
+    "url": "/smoothchords/index.html"
+  },
+  {
+    "revision": "2bda0ef195daa2afbee6",
+    "url": "/smoothchords/static/css/main.a1818423.chunk.css"
+  },
+  {
+    "revision": "ef7dde565425938a53c4",
+    "url": "/smoothchords/static/js/2.701d317d.chunk.js"
+  },
+  {
+    "revision": "2bda0ef195daa2afbee6",
+    "url": "/smoothchords/static/js/main.80b31ff4.chunk.js"
+  },
+  {
+    "revision": "9124eeb12f81febb7e44",
+    "url": "/smoothchords/static/js/runtime~main.79b5f788.js"
+  }
+]);
